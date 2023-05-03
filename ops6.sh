dd25058b1ba969c1a79748ad8ebb171c41b43cf0
tprloucks1010 <louckstraining@gmail.com>
2023-05-03 18:58:44 -0500

    ops6

---
+++ ops6.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+
+#Array of directories
+directories=["dir1" "dir2" "dir3"]
+
+#Loop through the array
+for directory in "${directories[@]}"; do
+#Start if statement. ! -d is checking the condition to see if the directory exists. If it does not it will create a directory. 
+if [! -d "$directories"]; then
+#creating the directory
+mkdir "$directory"
+#if directory does exists print to terminal "directory already exists"
+else 
+echo "directory exists"
+#ending if statement
+fi
+#ending function
+done
